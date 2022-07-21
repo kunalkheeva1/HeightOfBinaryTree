f4e6d72dbcb1b7a0e8cce6d88b589642171709e0
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 20:19:18 -0700

    coded a node class

--- src/HeightOfBinaryTree.java
+++ src/HeightOfBinaryTree.java
@@ -1,5 +1,20 @@
 public class HeightOfBinaryTree {
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
