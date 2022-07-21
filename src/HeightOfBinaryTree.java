ca5d94a6a243c7f8c28134f0e2b8ff404e7d6cf6
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 20:22:57 -0700

    coded the method to find the height of a binary tree

--- src/HeightOfBinaryTree.java
+++ src/HeightOfBinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HeightOfBinaryTree {
     class Node{
         int data;
@@ -9,7 +12,27 @@ public class HeightOfBinaryTree {
             this.right = right;
         }
     }
+    static int heightOfBinaryTree(Node root){
+        if(root == null) return 0;
+        int level =0;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
 
+        while(!q.isEmpty()){
+            int size = q.size();
+            level++;
+            while(size>0){
+                Node current = q.remove();
+                if(current.left != null){
+                    q.add(current.left);
+                }if(current.right != null){
+                    q.add(current.right);
+                }
+                    size--;
+            }
+        }return level;
+
+    }
 
 
 
